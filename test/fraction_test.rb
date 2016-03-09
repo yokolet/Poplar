f7b590347d84909c5dfd97b01108b0dfd29832ca
monkstone <martin_p@lineone.net>
2016-03-09 06:20:41 +0000

    a more elegant test

--- test/fraction_test.rb
+++ test/fraction_test.rb
@@ -10,9 +10,10 @@ require_relative '../lib/commons_math_fraction'
 
 class FractionTest < Minitest::Test
 
-  attr_reader :fraction
+  attr_reader :fraction, :half
   def setup
-    @fraction = Commons::Math::Fraction.new(1, 1)
+    @fraction = Commons::Math::Fraction.new(3, 16)
+    @half = Commons::Math::Fraction.new(1, 2)
   end
   
   def test_new
@@ -20,20 +21,20 @@ class FractionTest < Minitest::Test
   end
   
   def test_eql?
-    assert_equal fraction, Commons::Math::Fraction.new(1, 1), 'failed eql? test'
+    assert_equal fraction, Commons::Math::Fraction.new(3, 16), 'failed eql? test'
   end
   
   def test_refute_eql?
-    refute fraction.eql?(Commons::Math::Fraction.new(1, 2)), 'failed refute eql? test'
+    refute fraction.eql?(half), 'failed refute eql? test'
   end
   
   def test_add
-    assert_equal fraction.add(Commons::Math::Fraction.new(1, 16)), Commons::Math::Fraction.new(17, 16), 'failed add test'
+    assert_equal fraction.add(Commons::Math::Fraction.new(5, 16)), half, 'failed add test'
   end
   
   def test_add_bang
-    fraction.add!(Commons::Math::Fraction.new(1, 16))
-    assert_equal fraction, Commons::Math::Fraction.new(17, 16), 'failed add_bang test'
+    fraction.add!(Commons::Math::Fraction.new(5, 16))
+    assert_equal fraction, half, 'failed add_bang test'
   end
 end
 
